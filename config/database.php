e1f536411a88a5d929257a94c60775e71c3f55cd
WILLOSU <WILLOSU>
2024-12-03 21:17:30 -0300

    Banco de Dados SQL SERVER criado

--- config/database.php
+++ config/database.php
@@ -97,22 +97,23 @@ return [
             'sslmode' => 'prefer',
         ],
 
-        'sqlsrv' => [
+          'sqlsrv' => [
             'driver' => 'sqlsrv',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'localhost\SQL2022'),
             'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
+            'database' => env('DB_DATABASE', 'GestaoSelos'),
+            'username' => env('DB_USERNAME', 'willi'),
+            'password' => env('DB_PASSWORD', '1'),
+            'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-            // 'encrypt' => env('DB_ENCRYPT', 'yes'),
-            // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
+            'encrypt' => env('DB_ENCRYPT', 'yes'),
+            'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'true'),
         ],
 
     ],
+
+
 
     /*
     |--------------------------------------------------------------------------
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/test-db', function () {
+    try {
+        DB::connection()->getPdo();
+        return "Conexão com o banco de dados bem-sucedida!";
+    } catch (\Exception $e) {
+        return "Erro na conexão com o banco de dados: " . $e->getMessage();
+    }
 });
+
